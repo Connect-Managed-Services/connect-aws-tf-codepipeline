c6fce0a8d97b15c461dd50be3f61b9116ac24028
rambalakrish <105067391+rambalakrish@users.noreply.github.com>
2022-09-20 10:08:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,8 @@ locals {
 
 module "ec2" {
   #source            = "../../modules/e2esa-module-aws-ec2"
-  source             = "git::https://github.com/e2eSolutionArchitect/terraform.git//providers/aws/modules/e2esa-module-aws-ec2?ref=main"
+  #source             = "git::https://github.com/e2eSolutionArchitect/terraform.git//providers/aws/modules/e2esa-module-aws-ec2?ref=main"
+  source             = "git::https://github.com/Connect-Managed-Services/connect-aws-terraform-demo.git//providers/aws/modules/e2esa-module-aws-ec2?ref=main"
   #for_each          = toset(var.ec2_names) # toset(["ec21","ec22"])
   ami               = var.ami
   availability_zone = var.availability_zone
